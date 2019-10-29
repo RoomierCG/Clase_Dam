7e9ac8fe9a6121f859610a34261401356d007fac
Edgar Alexander Calle Granda <alex.comcg@gmail.com>
2019-10-29 14:25:39 +0100

    He agregao interfaz a la Calculadora, solo falta la parte logicca

--- Aplicaciones/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
+++ Aplicaciones/Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -3,12 +3,40 @@ package com.example.calculadora;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static int resultado;
+    public static int buffer = 0;
+    public static int cero;
+    public static int uno;
+    public static int dos;
+    public static int tres;
+    public static int cuatro;
+    public static int cinco;
+    public static int seis;
+    public static int siete;
+    public static int ocho;
+    public static int nueve;
+    /*Varibables*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void uno(View view){
+
+        
+        actualizarTexto(view);
+    }
+
+    public void actualizarTexto(View view){
+        TextView textView = (TextView) findViewById(R.id.textresultado);
+        textView.setText(Integer.toString(buffer));
+    }
+
 }
