382cfc36913b8f857db79ff38fd0ac91ed913c8f
Edgar Alexander Calle Granda <alex.comcg@gmail.com>
2019-11-05 11:17:53 +0100

    La calculara IQ esta casi terminada solo falta hacer el boton borrar y la cientifica

--- Aplicaciones/CalculadoraIQ/app/src/main/java/com/example/calculadoraiq/MainActivity.java
+++ Aplicaciones/CalculadoraIQ/app/src/main/java/com/example/calculadoraiq/MainActivity.java
@@ -9,6 +9,9 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     public static double buffer = 0;
+    public static double buffer2 = 0;
+    public static double buffer3 = 0;
+    public static int operador = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    /*                    Botones sumar                    */
+    /********************** BOTONES ************************/
     public void cero(View view){
         buffer *= 10;
         actualizarTexto(view);
@@ -67,9 +70,138 @@ public class MainActivity extends AppCompatActivity {
         actualizarTexto(view);
     }
 
+    /********************** OPERACIONES LOGICAS ************************/
+
+    public void C(View view){
+        buffer = 0;
+        buffer2 = 0;
+        actualizarTexto(view);
+    }
+
+    public void DEL(View view){
+        buffer /= 10;
+        //Falta por terminar
+        actualizarTexto(view);
+    }
+
+    public void suma(View view){
+        operador = 1;
+        buffer2 = buffer;
+        buffer = 0;
+        actualizarTexto(view);
+    }
+
+    public void resta(View view){
+        operador = 2;
+        buffer2 = buffer;
+        buffer = 0;
+        actualizarTexto(view);
+    }
+
+    public void multiplicacion (View view){
+        operador = 3;
+        buffer2 = buffer;
+        buffer = 0;
+        actualizarTexto(view);
+    }
+
+    public void division(View view){
+        operador = 4;
+        buffer2 = buffer;
+        buffer = 0;
+        actualizarTexto(view);
+    }
+
+    public void negapositivo(View view){
+        buffer *= -1;
+        actualizarTexto(view);
+    }
+
+    public void resultado(View view){
+
+        double resultado;
+        //variables locales
+
+        switch (operador){
+            case 1:
+                resultado = buffer + buffer2;
+                buffer3 = buffer;
+                buffer = resultado;
+                actualizarTexto(view);
+                buffer = 0;
+                operador = 0;
+                buffer3 = 0;
+                break;
+            case 2:
+                resultado = buffer - buffer2;
+                buffer = resultado;
+                actualizarTexto(view);
+                buffer = 0;
+                operador = 0;
+                buffer3 = 0;
+                break;
+            case 3:
+                resultado = buffer * buffer2;
+                buffer = resultado;
+                actualizarTexto(view);
+                buffer = 0;
+                operador = 0;
+                buffer3 = 0;
+                break;
+            case 4:
+                resultado = buffer / buffer2;
+                buffer = resultado;
+                actualizarTexto(view);
+                buffer = 0;
+                operador = 0;
+                buffer3 = 0;
+                break;
+        }
+    }
+
+    /********************** TextView ************************/
+
     public void actualizarTexto(View view){
         TextView textView = (TextView) findViewById(R.id.textresultado);
+        TextView textView1 = (TextView) findViewById(R.id.buffer);
+
         textView.setText(Double.toString(buffer));
+
+        //Hacemos que si le hemos dado al boton de suma o resta el textview buffer no tendra simbolo
+        if (operador != 1 && operador != 2 && operador != 3 && operador != 4){
+            textView1.setText(Double.toString(buffer2));
+        }else{
+            switch (operador){
+                case 1:
+                    textView1.setText(Double.toString(buffer2) + " + " + Double.toString(buffer));
+                    break;
+                case 2:
+                    textView1.setText(Double.toString(buffer2) + " - " + Double.toString(buffer));
+                    break;
+                case 3:
+                    textView1.setText(Double.toString(buffer2) + " * " + Double.toString(buffer));
+                    break;
+                case 4:
+                    textView1.setText(Double.toString(buffer2) + " / " + Double.toString(buffer));
+                    break;
+            }
+        }
+        if  (operador != 1 && operador != 2 && operador != 3 && operador != 4 || buffer3 != 0){
+            switch (operador){
+                case 1:
+                    textView1.setText(Double.toString(buffer2) + " + " + Double.toString(buffer3) + " = " + Double.toString(buffer));
+                    break;
+                case 2:
+                    textView1.setText(Double.toString(buffer2) + " - " + Double.toString(buffer3) + " = " + Double.toString(buffer));
+                    break;
+                case 3:
+                    textView1.setText(Double.toString(buffer2) + " * " + Double.toString(buffer3) + " = " + Double.toString(buffer));
+                    break;
+                case 4:
+                    textView1.setText(Double.toString(buffer2) + " / " + Double.toString(buffer3) + " = " + Double.toString(buffer));
+                    break;
+            }
+        }
     }
 
 }
