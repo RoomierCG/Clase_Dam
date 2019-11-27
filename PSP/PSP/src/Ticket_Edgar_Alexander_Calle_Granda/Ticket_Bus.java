11cb90beb2a48b71d6aa0d40be4553dd691fbb88
Edgar Alexander Calle Granda <alex.comcg@gmail.com>
2019-11-27 13:41:23 +0100

    Muchos cambios

--- PSP/PSP/src/Ticket_Edgar_Alexander_Calle_Granda/Ticket_Bus.java
+++ PSP/PSP/src/Ticket_Edgar_Alexander_Calle_Granda/Ticket_Bus.java
@@ -5,8 +5,7 @@ public class Ticket_Bus extends Tickets_operation{
 	public Ticket_Bus(int quantity_car,String character){
 		super(quantity_car,character);
 	}
-	
-	
+		
 	@Override
 	public int quantity_transport() {
 		return super.quantity_transport();
---
+++ PSP/PSP/src/test/h.java
@@ -0,0 +1,8 @@
+package test;
+
+public class h {
+	public static void main(String[] args){
+		String nombre = args[0];
+		System.out.println("hika to"+ nombre);
+	}
+}
