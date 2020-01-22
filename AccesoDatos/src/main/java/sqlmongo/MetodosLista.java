5f0223af820736d744a55cb5cf7df6b4591260e7
fernanix <fer99rivas@gmail.com>
2020-01-22 20:41:52 +0100

    Este commit deberia meter MetodosLista en el branch

--- AccesoDatos/src/main/java/sqlmongo/MetodosLista.java
+++ AccesoDatos/src/main/java/sqlmongo/MetodosLista.java
@@ -8,35 +8,37 @@ import org.bson.Document;
 import java.util.List;
 
 public class MetodosLista {
-    public static Alumno busqueda(int id, List<Alumno> alumnos){
+    public static Alumno busqueda(int id, List<Alumno> alumnos) {
         Alumno retorno = null;
-        for(Alumno i: alumnos){
-            if(i.getId()==id){
+        for (Alumno i : alumnos) {
+            if (i.getId() == id) {
                 retorno = i;
             }
         }
         return retorno;
     }
-    public static int extractPosicion(int id, List<Alumno> alumnos){
+
+    public static int extractPosicion(int id, List<Alumno> alumnos) {
         int result = -1;
-        for(int i = 0;i!=alumnos.size();i++){
-            if(alumnos.get(i).getId()==id){
+        for (int i = 0; i != alumnos.size(); i++) {
+            if (alumnos.get(i).getId() == id) {
                 result = i;
             }
         }
         return result;
     }
-    public static List<Alumno> busqueda(String apellido, List<Alumno> alumnos){
+
+    public static List<Alumno> busqueda(String apellido, List<Alumno> alumnos) {
         List<Alumno> retorno = null;
-        for(Alumno i: alumnos){
-            if(i.getApellido().equals(apellido)){
+        for (Alumno i : alumnos) {
+            if (i.getApellido().equals(apellido)) {
                 retorno.add(i);
             }
         }
         return retorno;
     }
 
-    public static boolean vacioNuevo(int id){
+    public static boolean vacioNuevo(int id) {
         MongoClient mongoClient = new MongoClient("localhost", 27017);
         MongoDatabase db = mongoClient.getDatabase("alumnos");
         MongoCollection collection = db.getCollection("libres");
@@ -45,9 +47,6 @@ public class MetodosLista {
         datosMongo = new DatosMongo();
         Document document = new Document();
         document.put("id", id);
-
+        return true;
     }
-
-
-
 }
